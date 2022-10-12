cb232a50cd13216d1a59a4bd71b7008876744e92
Ann-Annice <annannice98@gmail.com>
2022-10-12 21:59:55 +0530

    carsapp

---
+++ src/app/carsapp/carsapp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-carsapp',
+  templateUrl: './carsapp.component.html',
+  styleUrls: ['./carsapp.component.css']
+})
+export class CarsappComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
